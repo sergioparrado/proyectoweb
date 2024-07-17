05bd09f21a374e5ded599345d625d0e6e075e06d
Sergio Parrado Rojas <151929239+sergioparrado@users.noreply.github.com>
2024-07-17 11:28:17 -0500

    Add files via upload

---
+++ ProyectoWeb/ProyectoWeb/src/main/java/LoginServlet.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if ("admin".equals(username) && "admin123".equals(password)) {
+            out.println("<h3>Login Successful!</h3>");
+        } else {
+            out.println("<h3>Invalid username or password</h3>");
+        }
+    }
+}
